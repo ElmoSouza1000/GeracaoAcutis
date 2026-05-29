50a866df6c33f284058a341eda2186930ca263b0
Myllena Sales <myllenasales50@gmail.com>
2026-05-29 12:18:47 -0300

    ajustes e preenchimento dos cards

--- js/rezar.js
+++ js/rezar.js
@@ -1,22 +1,18 @@
-/*
-const nomeMisterios = {
+
+// Pega o ?misterio= da URL
+var misterio = new URLSearchParams(window.location.search).get('misterio');
+
+// Títulos
+var titulos = {
     gozosos: "Mistérios Gozosos",
     luminosos: "Mistérios Luminosos",
     dolorosos: "Mistérios Dolorosos",
     gloriosos: "Mistérios Gloriosos"
-}
-// Obtém o misterioso selecionado da URL (ex: ?misterio=gozosos e demais)
-const parametrosDaURL = new URLSearchParams(window.location.search);
-const misterioSelecionado = parametrosDaURL.get('misterio');
+};
 
-if(misterioSelecionado && videosPorMisterio[misterioSelecionado]) {
-    // Atualiza o título e o vídeo
-    document.getElementById('tituloMisterio').textContent = nomeMisterios[misterioSelecionado];
-    // Atualiza o vídeo do iframe
-    document.getElementById('videoMisterio').src = videosPorMisterio[misterioSelecionado];
-}else{
-    // Se o misterioso não for válido, exibe uma mensagem de erro
-    document.getElementById('tituloMisterio').textContent = "Misterioso não encontrado";
-    
+// Se achou o misterio na URL, mostra o título e o vídeo
+if (misterio && titulos[misterio]) {
+    document.getElementById('tituloMisterio').textContent = titulos[misterio];
+    document.getElementById('video-' + misterio).style.display = 'block';
 }
-*/
+
