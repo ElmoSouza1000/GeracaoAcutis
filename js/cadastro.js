902bc70d08bc97dab15164317807f29bcf60a2f3
Myllena Sales <myllenasales50@gmail.com>
2026-06-06 10:30:18 -0300

    verificarsenha

--- js/cadastro.js
+++ js/cadastro.js
@@ -1,4 +1,14 @@
 const btnCadastrar = document.querySelector("#btnCadastrar");
-btnCadastrar.addEventListener("click", () => {
-    window.location.href = "../comunidades.html";
-});
+const senha = document.querySelector("#cadSenha");
+const confirmarSenha = document.querySelector("#cadConfirmarSenha");
+const mensagemErro = document.querySelector("#erro");
+
+btnCadastrar.addEventListener("click", (event) =>{
+    if(senha.value !== confirmarSenha.value){
+        event.preventDefault();
+        mensagemErro.textContent = "As senhas não coincidem!";
+        return;
+    } else{
+            window.location.href = "../comunidades.html";
+    }
+})
